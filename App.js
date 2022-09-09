7614e5c21009400d8b0a8fa2d084f06e8f19b865
Murtaza0726 <murtazanaqvi0726@yahoo.com>
2022-09-08 21:12:09 -0400

    MyFirstApp

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>Welcome Murtaza Haider Naqvi !!</Text>
+      <Text style={styles.textStyle}>Your StudentID-301289355</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontSize: 25
+  }
 });
